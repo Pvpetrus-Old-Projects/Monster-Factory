49708553a86f4a084d5aa3fc54c20731e9d65531
pvpetrus <pvpetrus@wp.pl>
2021-06-12 00:01:48 +0200

    Creating Basic Skeletons and folders

---
+++ Monster_Kingdom/Mothers_Of_The_Swarm/Mother_Of_The_Storm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monster_Kingdom.Mothers_Of_The_Swarm
+{
+    class Mother_Of_The_Storm
+    {
+    }
+}
