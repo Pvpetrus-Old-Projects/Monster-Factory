99feea6299e206eed4930cfefb59fb7a5b2c121b
pvpetrus <pvpetrus@wp.pl>
2021-06-12 17:23:12 +0200

    All skeletons and getters done

---
+++ Monster_Kingdom/Monsters/Demon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monster_Kingdom.Shapers;
+namespace Monster_Kingdom.Monsters
+{
+    class Demon:Monster
+    {
+        public Demon():base()
+        {
+
+        }
+        public Demon(String race,int id,Double price,Shaper shaper):base(race,id,price,shaper)
+        {
+
+        }
+    }
+}
---
+++ Monster_Kingdom/Monsters/Ork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monster_Kingdom.Shapers;
+namespace Monster_Kingdom.Monsters
+{
+    class Ork:Monster
+    {
+        private String color { get; set; }
+        public Ork() : base()
+        {
+
+        }
+        public Ork(String race, int id, Double price, Shaper shaper,String color) : base(race, id, price, shaper)
+        {
+            this.color = color;
+        }
+    }
+}
